18b7a39f373738e3840490ec3c1a44eced3c2373
9c53e7e0fa44e1c3 <144809379+9c53e7e0fa44e1c3@users.noreply.github.com>
2024-11-22 16:13:42 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -6,7 +6,7 @@ import { SignJWT, jwtVerify } from 'jose';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '9b261477-c4ff-481c-a05f-05b728c2c2c7';
 let trojanPassword = `bpb-trojan`;
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
